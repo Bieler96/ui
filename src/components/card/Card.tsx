512de59781bba933b861ad0660f315958c510db4
D <bieler15147@live.de>
2025-07-28 22:26:22 +0200

    card

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from "clsx";
+
+export type CardProps = {
+	variant?: 'elevated' | 'filled' | 'outlined';
+	children: React.ReactNode;
+	className?: string;
+};
+
+const Card: React.FC<CardProps> = ({
+	variant = 'filled',
+	children,
+	className,
+}) => {
+	const baseClasses = 'rounded-lg p-4';
+
+	const variantClasses = {
+		elevated: 'shadow shadow-md',
+		filled: 'bg-surface-variant',
+		outlined: 'border border-outline-variant',
+	};
+
+	const cardClasses = clsx(baseClasses, variantClasses[variant], className);
+
+	return <div className={cardClasses}>{children}</div>;
+};
+
+export default Card;
---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Card from '../components/card/Card';
+
+const meta: Meta<typeof Card> = {
+	title: 'Components/Card',
+	component: Card,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	argTypes: {
+		variant: {
+			control: { type: 'select' },
+			options: ['elevated', 'filled', 'outlined'],
+		},
+	},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Elevated: Story = {
+	args: {
+		variant: 'elevated',
+		children: <p>This is an elevated card.</p>,
+	},
+};
+
+export const Filled: Story = {
+	args: {
+		variant: 'filled',
+		children: <p>This is a filled card.</p>,
+	},
+};
+
+export const Outlined: Story = {
+	args: {
+		variant: 'outlined',
+		children: <p>This is an outlined card.</p>,
+	},
+};
