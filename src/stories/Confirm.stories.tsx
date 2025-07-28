16ede94d61b4e9e1936f8fa4f9c636973f04666b
D <bieler15147@live.de>
2025-07-28 21:36:25 +0200

    confirm story

--- src/stories/Confirm.stories.tsx
+++ src/stories/Confirm.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Confirm } from './Confirm';
 import { useState } from 'react';
-import { Button } from '../button/Button';
+import { Confirm } from '../components/confirm/Confirm';
+import Button from '../components/button/Button';
 
 const meta: Meta<typeof Confirm> = {
   title: 'Components/Confirm',
