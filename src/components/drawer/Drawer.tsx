6d016feb9d03b21050a4e828370cb738cb1eacf1
D <bieler15147@live.de>
2025-07-29 18:22:12 +0200

    drawer

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -1,31 +1,46 @@
-import { Drawer } from 'vaul';
-import React, { Children } from 'react';
-
+import { Drawer } from "vaul";
+import clsx from "clsx";
 
 interface DrawerProps {
 	children: React.ReactNode;
 	open: boolean;
 	onOpenChange: (open: boolean) => void;
 	trigger: React.ReactNode;
+	direction?: "left" | "right" | "bottom";
 }
 
 export function CustomDrawer({
 	children,
 	open,
 	onOpenChange,
-	trigger
+	trigger,
+	direction = "bottom",
 }: DrawerProps) {
+	const contentBottomClass = "bg-surface flex flex-col rounded-t-lg mt-24 max-h-[96%] fixed bottom-0 left-0 right-0 outline-none";
+	const contentSideClass = "top-2 bottom-2 fixed z-10 outline-none w-[364px] max-w-[96%] flex rounded-lg";
+	const contentClass = direction === "bottom" ? contentBottomClass : contentSideClass;
+	const side = direction === "right" ? "right-2" : direction === "left" ? "left-2" : ""
+	const sideBg = direction === "right" || direction === "left" ? "bg-surface rounded-lg" : "mb-4";
+
 	return (
-		<Drawer.Root open={open} onOpenChange={onOpenChange}>
+		<Drawer.Root
+			open={open}
+			onOpenChange={onOpenChange}
+			direction={direction}
+		>
 			<Drawer.Trigger asChild>{trigger}</Drawer.Trigger>
 			<Drawer.Portal>
 				<Drawer.Overlay className="fixed inset-0 bg-black/40" />
-				<Drawer.Content className="bg-surface flex flex-col rounded-t-lg mt-24 max-h-[96%] fixed bottom-0 left-0 right-0 outline-none">
-					<div aria-hidden className="mt-4 mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-surface-variant mb-8" />
-					<div className="p-4 flex-1 overflow-y-auto">
-						<div className="max-w-md mx-auto space-y-4">
-							{children}
-						</div>
+				<Drawer.Content
+					className={clsx(contentClass, side)}
+					style={{ '--initial-transform': 'calc(100% + 8px)' } as React.CSSProperties}
+				>
+					{direction === 'bottom' ? (
+						<div aria-hidden className="my-4 mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-surface-variant" />
+					) : null}
+
+					<div className={clsx("p-2 flex-1 overflow-y-auto", sideBg)}>
+						{children}
 					</div>
 				</Drawer.Content>
 			</Drawer.Portal>
--- src/stories/Drawer.stories.tsx
+++ src/stories/Drawer.stories.tsx
@@ -12,7 +12,11 @@ const meta: Meta<typeof CustomDrawer> = {
 	tags: ['autodocs'],
 	argTypes: {
 		open: { control: 'boolean' },
-		onOpenChange: { action: 'openChange' }
+		onOpenChange: { action: 'openChange' },
+		direction: {
+			control: { type: 'select' },
+			options: ['left', 'right', 'bottom'],
+		},
 	},
 };
 
@@ -23,31 +27,59 @@ export const Default: Story = {
 	args: {
 		trigger: <Button>Open Drawer</Button>,
 		children: (
-			<div className="flex flex-col gap-4">
+			<div className="max-w-md mx-auto">
 				<p className="text-gray-600">
 					Nobody tells this to people who are beginners, I wish someone told me. All of us who do creative work,
 					we get into it because we have good taste.
 				</p>
-				<p className="text-gray-600">
-					But there is this gap. For the first couple years you make stuff, it’s just not that good. It’s trying
-					to be good, it has potential, but it’s not. But your taste, the thing that got you into the game, is
-					still killer. And your taste is why your work disappoints you. A lot of people never get past this
-					phase, they quit.{' '}
-				</p>
-				<p className="text-gray-600">
-					Most people I know who do interesting, creative work went through years of this. We know our work
-					doesn’t have this special thing that we want it to have. We all go through this. And if you are just
-					starting out or you are still in this phase, you gotta know its normal and the most important thing you
-					can do is do a lot of work
-				</p>
-				<p className="text-gray-600">
-					Put yourself on a deadline so that every week you will finish one story. It is only by going through a
-					volume of work that you will close that gap, and your work will be as good as your ambitions. And I took
-					longer to figure out how to do this than anyone I’ve ever met. It’s gonna take awhile. It’s normal to
-					take awhile. You’ve just gotta fight your way through.
-				</p>
 			</div>
 		)
+	},
+	render: (args) => {
+		const [open, setOpen] = React.useState(false);
+		return (
+			<CustomDrawer {...args} open={open} onOpenChange={setOpen}>
+				{args.children}
+			</CustomDrawer>
+		);
+	},
+};
+
+export const Left: Story = {
+	args: {
+		trigger: <Button>Open Drawer</Button>,
+		children: (
+			<div className="max-w-md mx-auto">
+				<p className="text-gray-600">
+					Nobody tells this to people who are beginners, I wish someone told me. All of us who do creative work,
+					we get into it because we have good taste.
+				</p>
+			</div>
+		),
+		direction: "left"
+	},
+	render: (args) => {
+		const [open, setOpen] = React.useState(false);
+		return (
+			<CustomDrawer {...args} open={open} onOpenChange={setOpen}>
+				{args.children}
+			</CustomDrawer>
+		);
+	},
+};
+
+export const Right: Story = {
+	args: {
+		trigger: <Button>Open Drawer</Button>,
+		children: (
+			<div className="max-w-md mx-auto">
+				<p className="text-gray-600">
+					Nobody tells this to people who are beginners, I wish someone told me. All of us who do creative work,
+					we get into it because we have good taste.
+				</p>
+			</div>
+		),
+		direction: "right"
 	},
 	render: (args) => {
 		const [open, setOpen] = React.useState(false);
